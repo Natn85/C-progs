0b27f676334ad45cbd8a10a6f588d0f8330e8f45
Natn85 <dnatnael928@gmail.com>
2025-04-28 08:28:46 +0200

    Add files via upload

---
+++ FPA/Gruppenwechsel/gruwe.c
@@ -0,0 +1,46 @@
+//Natnal Feleke - gruwe.c´
+
+#include <stdio.h>
+
+int split(char string[], char teil[], char trenner);
+
+int main (void)
+{
+	int i, j, k, lng, kunde, ;
+	char temp[100], knum[30], datum[30], ums[30], data[100][100];
+
+	FILE *gruwe = fopen("gruwe.txt", "r");
+	if (gruwe == NULL)
+	{
+		printf("Datei ist nicht gefunden\n");
+	}
+	else
+	{
+		FILE *output = fopen(
+		while(fgets(temp, 100, gruwe) != NULL)
+		{
+			i = 0;
+			lng = split(temp, knum, ';');
+			lng += split(temp + lng, datum, ';');
+			lng += split(temp + lng, ums, ';');
+			printf("%s, %s, %s\n", knum, datum, ums);
+
+			i ++;
+		}
+		fclose(gruwe);
+	}
+	return 0;
+}
+int split (char string[], char teil[], char trenner)
+{
+	int i = 0; 
+	while (string[i] != trenner && string[i] != '\0' && string[i] != '\n')
+	{
+		teil [i] = string[i];
+		i ++;
+	}
+	teil[i] = '\0';
+	i ++;
+	return i;
+}
+
