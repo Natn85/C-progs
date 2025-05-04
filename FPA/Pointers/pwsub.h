a89789057962803ab0b959e65cb4db9f4a95dff0
Natn85 <dnatnael928@gmail.com>
2025-05-04 16:37:11 +0200

    Update pwsub.h

--- FPA/Pointers/pwsub.h
+++ FPA/Pointers/pwsub.h
@@ -3,18 +3,23 @@
 #include <stdio.h>
 
 void pot(int *a, int *b, int *c);
-void wur(int *a, float *b);
+void wur(float *a, float *b);
 
 void pot(int *a, int *b, int *c)
 {
-	int i = 1;
-	*c = *a;
-	for(i = 1; i < *b; i++)
-	{
-		*c = *c * *a;
-	}
+        int i = 1;
+        *c = *a;
+        for(i = 1; i < *b; i++)
+        {
+                *c = *c * *a;
+        }
 }
-void wur(int *a, float *b)
+void wur(float *a, float *b)
 {
-	*b = ((*a / 2)+(*a / (*a / 2))) / 2;
+        float x = *a / 2.0;
+        for(int i = 0; i < 5; i++)
+        {
+                x = 0.5 * (x + (*a / x));
+        }
+        *b = x;
 }
